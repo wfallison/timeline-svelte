feee3ee07c48349eb84db8be41f1d119661d068f
Billy Allison <williamfallison@gmail.com>
2022-04-26 22:08:54 -0700

    Adds 'country' file from example dropdown

---
+++ src/components/Country.svelte
@@ -0,0 +1,10 @@
+<script>
+    export let itemLabel;
+    export let highlighted;
+  </script>
+  
+  <style>
+
+  </style>
+  
+  <li class="autocomplete-items" class:autocomplete-active={highlighted} on:click>{@html itemLabel}</li>
